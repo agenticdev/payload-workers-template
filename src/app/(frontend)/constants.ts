05e7e3f46464d7812f8b97145529eba925d14724
Gunnar Saliev <g.saliev@gmail.com>
2025-10-24 21:23:33 +0300

    added constants file

---
+++ src/app/(frontend)/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_NAME = 'Patyload Workers Template'
+export const SITE_DESCRIPTION = 'This is a starter template for Payload CMS for Cloudflare Workers'
+export const SITE_URL =
+  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://example.com'
